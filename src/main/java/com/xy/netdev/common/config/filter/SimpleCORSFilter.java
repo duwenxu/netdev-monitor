f685e0c9b9d8056f3fa9a5f5f3f76995614513dc
luoqilong <792788115@qq.com>
2021-03-05 15:42:41 +0800

    网络设备监控

--- src/main/java/com/xy/netdev/common/config/filter/SimpleCORSFilter.java
+++ src/main/java/com/xy/netdev/common/config/filter/SimpleCORSFilter.java
@@ -15,7 +15,7 @@ public class SimpleCORSFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "*");
 
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
 
         response.setHeader("Access-Control-Max-Age", "3600");
 
