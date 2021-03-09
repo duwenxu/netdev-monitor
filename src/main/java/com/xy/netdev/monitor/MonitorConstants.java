7f736c5ad57bb4431a210e5c7ff0619c6c52b570
wenxu du <1462289331@qq.com>
2021-03-09 15:36:55 +0800

    设备导航菜单接口修改

---
+++ src/main/java/com/xy/netdev/monitor/MonitorConstants.java
@@ -0,0 +1,15 @@
+package com.xy.netdev.monitor;
+
+/**
+*
+* @Description: 设备监控常量类
+* @Date 15:30 2021/3/9
+* @Author duwx
+*/
+public interface MonitorConstants {
+
+    /**
+     * 设备状态正常
+     */
+    String BASE_INFO_AVAILABLE = "0028001";
+}
