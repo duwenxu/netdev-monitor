9e334b31ff4f0276e9ede256f629dc63f5cf4270
tangxl <txl0915@sina.com>
2021-03-10 10:57:29 +0800

    提交工厂类

--- src/main/java/com/xy/netdev/factory/QueryInterPrtcllFactory.java
+++ src/main/java/com/xy/netdev/factory/QueryInterPrtcllFactory.java
@@ -3,25 +3,24 @@ package com.xy.netdev.factory;
 
 
 import com.xy.netdev.common.util.SpringContextUtils;
-import com.xy.netdev.frame.service.IParaPrtclAnalysisService;
-
+import com.xy.netdev.frame.service.IQueryInterPrtclAnalysisService;
 
 
 /**
  * <p>
- * 参数协议解析工厂类
+ * 查询接口协议解析工厂类
  * </p>
  *
  * @author tangxl
  * @since 2021-03-10
  */
-public class ParaPrtclFactory {
+public class QueryInterPrtcllFactory {
     /**
      * @功能：生成具体的处理类  传入为空时，返回缺省的处理类
      * @param  handlerMark           处理服务名
      * @return
      */
-    public static IParaPrtclAnalysisService genHandler(String handlerMark){
+    public static IQueryInterPrtclAnalysisService genHandler(String handlerMark){
        return SpringContextUtils.getBean(handlerMark);
     }
 }
