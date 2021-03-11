720076d95f8ab38270fa2769ca3ade8b49a26964
sunchao <1306519243@qq.com>
2021-03-11 14:45:08 +0800

    容器代码

--- src/main/java/com/xy/netdev/container/BaseInfoContainer.java
+++ src/main/java/com/xy/netdev/container/BaseInfoContainer.java
@@ -245,11 +245,11 @@ public class BaseInfoContainer {
     /**
      * @功能：根据设备类型  和  接口编码 获取参数列表
      * @param devType     设备类型
-     * @param itfCode     接口编码
+     * @param cmdMark     命令标识
      * @return  接口解析参数列表
      */
-    public static List<FrameParaInfo> getInterLinkParaList(String devType,String itfCode){
-        DevInterParam devInterParam = InterLinkParaMap.get(ParaHandlerUtil.genLinkKey(devType,itfCode));
+    public static List<FrameParaInfo> getInterLinkParaList(String devType,String cmdMark){
+        DevInterParam devInterParam = InterLinkParaMap.get(ParaHandlerUtil.genLinkKey(devType,cmdMark));
         if(devInterParam != null){
             return devInterParam.getDevParamList();
         }
