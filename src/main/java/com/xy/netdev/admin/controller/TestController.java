243b57f3aa049fbfe271cddbceb48f170ce436d6
wenxu du <1462289331@qq.com>
2021-03-18 09:47:02 +0800

    单个参数响应调试

---
+++ src/main/java/com/xy/netdev/admin/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.xy.netdev.admin.controller;
+
+import com.xy.netdev.transit.schedule.ScheduleQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试
+ *
+ * @author duwenxu
+ * @create 2021-03-17 15:27
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+//    @Autowired
+//    private ScheduleQuery scheduleQuery;
+//
+//    @RequestMapping(value = "/query", method = RequestMethod.POST)
+//    public void queryPageList() {
+//        scheduleQuery.doScheduleQuery();
+//    }
+}
