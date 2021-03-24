8708ee0c369d972cb67f9c914129473f241363ac
wenxu du <1462289331@qq.com>
2021-03-24 09:04:38 +0800

    暂存400W功放解析

--- src/main/java/com/xy/netdev/frame/service/impl/head/PowerAmpImpl.java
+++ src/main/java/com/xy/netdev/frame/service/impl/head/PowerAmpImpl.java
@@ -1,111 +1,194 @@
-//package com.xy.netdev.frame.service.impl.head;
-//
-//import cn.hutool.core.util.HexUtil;
-//import com.xy.netdev.container.BaseInfoContainer;
-//import com.xy.netdev.frame.base.AbsDeviceSocketHandler;
-//import com.xy.netdev.frame.bo.FrameReqData;
-//import com.xy.netdev.frame.bo.FrameRespData;
-//import com.xy.netdev.frame.entity.SocketEntity;
-//import com.xy.netdev.frame.entity.device.PowerAmpEntity;
-//import com.xy.netdev.frame.service.IParaPrtclAnalysisService;
-//import com.xy.netdev.frame.service.IQueryInterPrtclAnalysisService;
-//import com.xy.netdev.monitor.entity.BaseInfo;
-//import com.xy.netdev.monitor.entity.PrtclFormat;
-//import io.netty.buffer.ByteBuf;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//
-//import static com.xy.netdev.common.constant.SysConfigConstant.OPREATE_CONTROL_RESP;
-//import static com.xy.netdev.common.constant.SysConfigConstant.OPREATE_QUERY_RESP;
-//import static com.xy.netdev.common.util.ByteUtils.*;
-//
-///**
-// * Ku4000W功率放大器
-// *
-// * @author duwenxu
-// * @create 2021-03-22 15:23
-// */
-//@Service
-//@Slf4j
-//public class PowerAmpImpl extends AbsDeviceSocketHandler<SocketEntity, FrameReqData, FrameRespData> {
-//
-//    /**
-//     * 数据接收解析回调
-//     *
-//     * @param frameRespData                   数据帧结构与
-//     * @param iParaPrtclAnalysisService       参数响应处理类
-//     * @param iQueryInterPrtclAnalysisService 接口响应处理类
-//     */
-//    @Override
-//    public void callback(FrameRespData frameRespData, IParaPrtclAnalysisService iParaPrtclAnalysisService, IQueryInterPrtclAnalysisService iQueryInterPrtclAnalysisService) {
-//        String operType = frameRespData.getOperType();
-//        switch (operType) {
-//            case OPREATE_QUERY_RESP:
-//                iQueryInterPrtclAnalysisService.queryParaResponse(frameRespData);
-//                break;
-//            case OPREATE_CONTROL_RESP:
-//                iParaPrtclAnalysisService.ctrlParaResponse(frameRespData);
-//                break;
-//            default:
-//                log.warn("设备:{},未知的参数类型:{}", frameRespData.getDevNo(), frameRespData.getCmdMark());
-//                break;
-//        }
-//    }
-//
-//    @Override
-//    public FrameRespData unpack(SocketEntity socketEntity, FrameRespData frameRespData) {
-//        byte[] bytes = socketEntity.getBytes();
-//        if (bytes.length <= 8) {
-//            log.warn("Ku4000W功率放大器响应数据长度错误, 未能正确解析, 数据体长度:{}, 数据体:{}", bytes.length, HexUtil.encodeHexStr(bytes));
-//            return frameRespData;
-//        }
-//        //按协议中指定的位置获取数据体长度
-//        int len = bytesToNum(bytes, 4, 2, ByteBuf::readShort);
-//        //响应类型标识
-//        Byte respType = bytesToNum(bytes, 7, 1, ByteBuf::readByte);
-//        String hexRespType = numToHexStr(Long.valueOf(respType));
-//        //参数关键字
-//        Byte cmd = bytesToNum(bytes, 8, 1, ByteBuf::readByte);
-//        String hexCmd = numToHexStr(Long.valueOf(cmd));
-//        //数据体
-//        byte[] paramBytes = byteArrayCopy(bytes, 6, len);
-//        //业务数据结构包装
-//        PrtclFormat prtclFormat = BaseInfoContainer.getPrtclByInterfaceOrPara(frameRespData.getDevType(), hexCmd);
-//        String operateType = BaseInfoContainer.getOptByPrtcl(prtclFormat, hexRespType);
-//        frameRespData.setOperType(operateType);
-//        frameRespData.setCmdMark(hexCmd);
-//        frameRespData.setParamBytes(paramBytes);
-//        return frameRespData;
-//    }
-//
-//    @Override
-//    public byte[] pack(FrameReqData frameReqData) {
-//        //参数数据
-//        byte[] paramBytes = frameReqData.getParamBytes();
-//        //数据长度
-//        int dataLength = 0;
-//        if (paramBytes != null) {
-//            //命令字+参数体 之外的固定长度 9
-//            dataLength = paramBytes.length + 9;
-//        }
-//        String subTypeCode = getDevSubCode(frameReqData);
-//        PowerAmpEntity.builder()
-//                .beginOffset((byte) 0x7E)
-//                .devType((byte) 0x21)
-//                .devSubType(Byte.valueOf(subTypeCode, 16))
-//                .deviceAddress(new byte[]{0x00,0x01})
-//                .length(byt)
-//                .cmd(paramBytes)
-//                .params((byte) 0)
-//                .check((byte) 0x7D)
-//                .end()
-//                .build();
-//        return new byte[0];
-//    }
-//
-//    private String getDevSubCode(FrameReqData frameReqData) {
-//        BaseInfo baseInfo = BaseInfoContainer.getDevInfoByNo(frameReqData.getDevNo());
-//        String devSubType = baseInfo.getDevSubType();
-//        if ()
-//    }
-//}
+package com.xy.netdev.frame.service.impl.head;
+
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.HexUtil;
+import cn.hutool.core.util.NumberUtil;
+import com.google.common.base.Charsets;
+import com.xy.common.exception.BaseException;
+import com.xy.netdev.admin.service.ISysParamService;
+import com.xy.netdev.common.util.ByteUtils;
+import com.xy.netdev.container.BaseInfoContainer;
+import com.xy.netdev.frame.base.AbsDeviceSocketHandler;
+import com.xy.netdev.frame.bo.FrameReqData;
+import com.xy.netdev.frame.bo.FrameRespData;
+import com.xy.netdev.frame.entity.SocketEntity;
+import com.xy.netdev.frame.entity.device.AntennaControlEntity;
+import com.xy.netdev.frame.entity.device.PowerAmpEntity;
+import com.xy.netdev.frame.service.IParaPrtclAnalysisService;
+import com.xy.netdev.frame.service.IQueryInterPrtclAnalysisService;
+import com.xy.netdev.monitor.entity.BaseInfo;
+import com.xy.netdev.monitor.entity.PrtclFormat;
+import io.netty.buffer.ByteBuf;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.xy.netdev.common.constant.SysConfigConstant.*;
+import static com.xy.netdev.common.util.ByteUtils.*;
+
+/**
+ * Ku4000W功率放大器
+ *
+ * @author duwenxu
+ * @create 2021-03-22 15:23
+ */
+@Service
+@Slf4j
+public class PowerAmpImpl extends AbsDeviceSocketHandler<SocketEntity, FrameReqData, FrameRespData> {
+
+    @Autowired
+    private ISysParamService sysParamService;
+    /**查询/控制响应命令标识*/
+    private static final String QUERY_RES = "83";
+    private static final String CONTROL_RES = "81";
+    private static final String QUERY_CMD ="82";
+
+    /**
+     * 数据接收解析回调
+     *
+     * @param frameRespData                   数据帧结构与
+     * @param iParaPrtclAnalysisService       参数响应处理类
+     * @param iQueryInterPrtclAnalysisService 接口响应处理类
+     */
+    @Override
+    public void callback(FrameRespData frameRespData, IParaPrtclAnalysisService iParaPrtclAnalysisService, IQueryInterPrtclAnalysisService iQueryInterPrtclAnalysisService) {
+        String operType = frameRespData.getOperType();
+        switch (operType) {
+            case OPREATE_QUERY_RESP:
+                iQueryInterPrtclAnalysisService.queryParaResponse(frameRespData);
+                break;
+            case OPREATE_CONTROL_RESP:
+                iParaPrtclAnalysisService.ctrlParaResponse(frameRespData);
+                break;
+            default:
+                log.warn("设备:{},未知的参数类型:{}", frameRespData.getDevNo(), frameRespData.getCmdMark());
+                break;
+        }
+    }
+
+    @Override
+    public FrameRespData unpack(SocketEntity socketEntity, FrameRespData frameRespData) {
+        byte[] bytes = socketEntity.getBytes();
+        if (bytes.length <= 8) {
+            log.warn("Ku4000W功率放大器响应数据长度错误, 未能正确解析, 数据体长度:{}, 数据体:{}", bytes.length, HexUtil.encodeHexStr(bytes));
+            return frameRespData;
+        }
+        //按协议中指定的位置获取数据体长度
+        int len = bytesToNum(bytes, 5, 2, ByteBuf::readShort);
+        int hexLen = Integer.parseInt(HexUtil.toHex(len));
+        //响应类型标识
+        Short respType = bytesToNum(bytes, 7, 1, ByteBuf::readUnsignedByte);
+        String hexRespType = HexUtil.toHex(respType);
+
+        //数据体
+        byte[] paramBytes = byteArrayCopy(bytes, 8, hexLen);
+        //判断操作类型赋值
+        if (QUERY_RES.equals(hexRespType)){
+            frameRespData.setCmdMark(QUERY_CMD);
+            frameRespData.setOperType(OPREATE_QUERY_RESP);
+        }else if (CONTROL_RES.equals(hexRespType)){
+            frameRespData.setOperType(OPREATE_CONTROL_RESP);
+            //参数关键字
+            Byte cmd = bytesToNum(bytes, 8, 1, ByteBuf::readByte);
+            String hexCmd = numToHexStr(Long.valueOf(cmd));
+            frameRespData.setCmdMark(hexCmd);
+        }
+        frameRespData.setParamBytes(paramBytes);
+        return frameRespData;
+    }
+
+    @Override
+    public byte[] pack(FrameReqData frameReqData) {
+        //参数数据
+        byte[] paramBytes = frameReqData.getParamBytes();
+        //数据长度
+        int dataLength = 1;
+        if (paramBytes != null) {
+            //命令字+参数体 之外的固定长度 9
+            dataLength = paramBytes.length;
+        }
+        String subTypeCode = getDevSubCode(frameReqData);
+        String operType = frameReqData.getOperType();
+        String keyWord;
+        PrtclFormat prtclFormat = BaseInfoContainer.getPrtclByInterfaceOrPara(frameReqData.getDevType(), frameReqData.getCmdMark());
+        if (prtclFormat==null|| prtclFormat.getFmtId()==null){
+            throw new BaseException("设备类型为"+frameReqData.getDevType()+"，参数命令为"+frameReqData.getCmdMark()+"协议格式获取失败...");
+        }
+        if (OPREATE_QUERY.equals(operType)){
+            keyWord = prtclFormat.getFmtSkey();
+        }else {
+            keyWord = prtclFormat.getFmtCkey();
+        }
+        if (StringUtils.isBlank(subTypeCode)){
+            throw new BaseException("编号为"+frameReqData.getDevNo()+"子设备编码为null...");
+        }
+        PowerAmpEntity ampEntity = PowerAmpEntity.builder()
+                .beginOffset((byte) 0x7E)
+                .devType((byte) 0x21)
+                .devSubType(Byte.valueOf(subTypeCode,16))
+                .deviceAddress(new byte[]{0x00, 0x01})
+                .length(objToBytes(dataLength, 2))
+                .cmd((byte)Integer.parseInt(keyWord,16))
+                .params(paramBytes)
+                .end((byte) 0x7E)
+                .build();
+        //todo 校验合
+        byte checkSum = xor(ampEntity);
+        ampEntity.setCheck(checkSum);
+        return pack(ampEntity);
+    }
+
+    private byte[] pack(PowerAmpEntity ampEntity) {
+        List<byte[]> list = new ArrayList<>();
+        list.add(new byte[]{ampEntity.getBeginOffset()});
+        list.add(new byte[]{ampEntity.getDevType()});
+        list.add(new byte[]{ampEntity.getDevSubType()});
+        list.add(ampEntity.getDeviceAddress());
+        list.add(ampEntity.getLength());
+        list.add(new byte[]{ampEntity.getCmd()});
+        if (ampEntity.getParams() != null){
+            list.add(ampEntity.getParams());
+        }
+        list.add(new byte[]{ampEntity.getCheck()});
+        list.add(new byte[]{ampEntity.getEnd()});
+        return listToBytes(list);
+    }
+
+    private byte xor(PowerAmpEntity entity){
+        byte[] bytes = {entity.getCmd()};
+        byte[] xorBytes = ArrayUtil.addAll(bytes, entity.getDeviceAddress(),entity.getLength());
+        if (entity.getParams() != null){
+            xorBytes = ArrayUtil.addAll(xorBytes,entity.getParams());
+        }
+        return ByteUtils.xor(xorBytes, 0, xorBytes.length);
+    }
+
+    /**
+     * 获取子设备型号 编码
+     * @param frameReqData 数据结构
+     * @return 子设备编码
+     */
+    private String getDevSubCode(FrameReqData frameReqData) {
+        BaseInfo baseInfo = BaseInfoContainer.getDevInfoByNo(frameReqData.getDevNo());
+        String devSubType = baseInfo.getDevSubType();
+        if (devSubType!=null){
+            return sysParamService.getParaRemark1(devSubType);
+        }
+        return null;
+    }
+
+    /**
+     * 数组转十六进制字符串并补全
+     * @param num
+     * @return
+     */
+    private static String lefPadNumToHexStr(long num){
+        return StringUtils.leftPad(HexUtil.toHex(num), 2,'0').toUpperCase();
+    }
+}
--- src/main/java/com/xy/netdev/frame/service/pam/PowerAmpInterPrtcServiceImpl.java
+++ src/main/java/com/xy/netdev/frame/service/pam/PowerAmpInterPrtcServiceImpl.java
@@ -1,10 +1,29 @@
 package com.xy.netdev.frame.service.pam;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
+import com.xy.netdev.admin.service.ISysParamService;
+import com.xy.netdev.common.util.ByteUtils;
+import com.xy.netdev.container.BaseInfoContainer;
+import com.xy.netdev.frame.bo.FrameParaData;
 import com.xy.netdev.frame.bo.FrameReqData;
 import com.xy.netdev.frame.bo.FrameRespData;
+import com.xy.netdev.frame.enums.ProtocolRequestEnum;
 import com.xy.netdev.frame.service.IQueryInterPrtclAnalysisService;
+import com.xy.netdev.frame.service.SocketMutualService;
+import com.xy.netdev.monitor.bo.FrameParaInfo;
+import com.xy.netdev.monitor.entity.PrtclFormat;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static com.xy.netdev.common.util.ByteUtils.byteToNumber;
+import static com.xy.netdev.frame.service.gf.GfPrtcServiceImpl.isUnsigned;
 
 /**
  * Ku400w功放 接口查询响应 帧协议解析层
@@ -16,13 +35,44 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class PowerAmpInterPrtcServiceImpl implements IQueryInterPrtclAnalysisService {
 
+    @Autowired
+    private SocketMutualService socketMutualService;
+    @Autowired
+    private ISysParamService sysParamService;
+    private static final String QUERY = "82";
+
     @Override
     public void queryPara(FrameReqData reqInfo) {
-
+        //获取查询关键字
+//        PrtclFormat prtclFormat = BaseInfoContainer.getPrtclByInterfaceOrPara(reqInfo.getDevType(), reqInfo.getCmdMark());
+//        String fmtSkey = prtclFormat.getFmtSkey();
+//        byte[] bytes = ByteUtils.objToBytes(fmtSkey, 1);
+//        reqInfo.setParamBytes(bytes);
+        socketMutualService.request(reqInfo, ProtocolRequestEnum.QUERY);
     }
 
     @Override
     public FrameRespData queryParaResponse(FrameRespData respData) {
-        return null;
+        List<FrameParaInfo> frameParaInfos = BaseInfoContainer
+                .getInterLinkParaList(respData.getDevType(), QUERY);
+        byte[] bytes = respData.getParamBytes();
+        if (ObjectUtil.isNull(bytes)){
+            log.warn("400W功放查询响应异常, 未获取到数据体, 信息:{}", JSON.toJSONString(respData));
+            return respData;
+        }
+        List<FrameParaData> frameParaDataList = frameParaInfos.stream()
+                .filter(Objects::nonNull)
+                .map(frameParaInfo -> {
+                    FrameParaData paraInfo = new FrameParaData();
+                    BeanUtil.copyProperties(frameParaInfo, paraInfo, true);
+                    BeanUtil.copyProperties(respData, paraInfo, true);
+                    paraInfo.setLen(Integer.parseInt(frameParaInfo.getParaByteLen()));
+                    paraInfo.setParaVal(byteToNumber(bytes, frameParaInfo.getParaStartPoint() - 1,
+                            Integer.parseInt(frameParaInfo.getParaByteLen()), isUnsigned(sysParamService,
+                                    frameParaInfo.getAlertPara())).toString());
+                    return paraInfo;
+                }).collect(Collectors.toList());
+        respData.setFrameParaList(frameParaDataList);
+        return respData;
     }
 }
