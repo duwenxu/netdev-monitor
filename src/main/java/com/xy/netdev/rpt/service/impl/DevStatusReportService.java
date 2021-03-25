1e308c04e172d8e46a8307f4a86aeff59a2632a6
luoqilong <792788115@qq.com>
2021-03-25 14:16:22 +0800

    发送请求失败设备中断状态上报

--- src/main/java/com/xy/netdev/rpt/service/impl/DevStatusReportService.java
+++ src/main/java/com/xy/netdev/rpt/service/impl/DevStatusReportService.java
@@ -133,8 +133,7 @@ public class DevStatusReportService implements IDevStatusReportService {
         //推送前端
         DevIfeMegSend.sendDevStatusToDev();
         //上报站控
-        //upRptPrtclAnalysisService.queryParaResponse(rptHeadDev);
-        System.out.println("------------------上报站控设备状态");
+        upRptPrtclAnalysisService.queryParaResponse(rptHeadDev);
     }
 
 
@@ -233,8 +232,7 @@ public class DevStatusReportService implements IDevStatusReportService {
                     .alertDesc(alertDesc).build();
             DevAlertInfoContainer.addAlertInfo(alertInfo);
             RptHeadDev rptHeadDev = crateRptHeadDev(alertInfo);
-            //upRptPrtclAnalysisService.queryParaResponse(rptHeadDev);
-            System.out.println("------------------上报站控设备告警");
+            upRptPrtclAnalysisService.queryParaResponse(rptHeadDev);
         }
     }
 
