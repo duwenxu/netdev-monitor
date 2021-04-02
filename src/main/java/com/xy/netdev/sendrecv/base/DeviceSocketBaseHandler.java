17cb484047aeeae30cca3653ee3e33bdd0459b3d
chenkai <436071185@qq.com>
2021-04-02 15:17:03 +0800

    netty TCP 添加超时重试机制

--- src/main/java/com/xy/netdev/sendrecv/base/DeviceSocketBaseHandler.java
+++ src/main/java/com/xy/netdev/sendrecv/base/DeviceSocketBaseHandler.java
@@ -9,9 +9,8 @@ import com.xy.netdev.frame.bo.FrameRespData;
  * 方法默认空实现
  * @author cc
  */
-public abstract class DeviceSocketBaseHandler<T extends FrameReqData, R extends FrameRespData> implements DeviceSocketService<T,
-        R>,
-        ProtocolActionService<T, R> {
+public abstract class DeviceSocketBaseHandler<T extends FrameReqData, R extends FrameRespData>
+        implements DeviceSocketService<T, R>, ProtocolActionService<T, R> {
 
 
 }
