4a22c58a1a941ac8aacc22fcbe2d1cf1b1d9bc7c
chenkai <436071185@qq.com>
2021-04-02 16:35:23 +0800

    /

--- src/main/java/com/xy/netdev/sendrecv/enums/CallbackTypeEnum.java
+++ src/main/java/com/xy/netdev/sendrecv/enums/CallbackTypeEnum.java
@@ -3,17 +3,6 @@ package com.xy.netdev.sendrecv.enums;
 import lombok.Getter;
 
 public enum CallbackTypeEnum {
-    DEFAULT("", "");
-    ;
-    CallbackTypeEnum(String key, String value){
-        this.key = key;
-        this.value = value;
-    }
-
-    @Getter
-    private String key;
-
-    @Getter
-    private String value;
+    DEFAULT;
 
 }
