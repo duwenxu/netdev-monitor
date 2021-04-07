be0d0d467ed00797c825f9a1e8d05cd68879ce71
sunchao <1306519243@qq.com>
2021-04-07 17:03:00 +0800

    C中频调测查询

---
+++ src/main/java/com/xy/netdev/frame/service/czp/CzpInterPrtcServiceImpl.java
@@ -0,0 +1,104 @@
+package com.xy.netdev.frame.service.czp;
+
+import cn.hutool.core.util.HexUtil;
+import com.xy.netdev.admin.service.ISysParamService;
+import com.xy.netdev.container.BaseInfoContainer;
+import com.xy.netdev.frame.bo.FrameParaData;
+import com.xy.netdev.frame.bo.FrameReqData;
+import com.xy.netdev.frame.bo.FrameRespData;
+import com.xy.netdev.frame.service.IQueryInterPrtclAnalysisService;
+import com.xy.netdev.frame.service.SocketMutualService;
+import com.xy.netdev.monitor.bo.FrameParaInfo;
+import com.xy.netdev.monitor.constant.MonitorConstants;
+import com.xy.netdev.sendrecv.enums.ProtocolRequestEnum;
+import com.xy.netdev.transit.IDataReciveService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import static com.xy.netdev.common.util.ByteUtils.byteToNumber;
+import static com.xy.netdev.frame.service.gf.GfPrtcServiceImpl.isFloat;
+import static com.xy.netdev.frame.service.gf.GfPrtcServiceImpl.isUnsigned;
+
+/**
+ * C中频切换矩阵
+ *
+ * @author sunchao
+ * @create 2021-04-07 09:30
+ */
+@Service
+@Slf4j
+public class CzpInterPrtcServiceImpl implements IQueryInterPrtclAnalysisService {
+
+    @Autowired
+    private SocketMutualService socketMutualService;
+    @Autowired
+    private ISysParamService sysParamService;
+    @Autowired
+    private IDataReciveService dataReciveService;
+    /**查询应答帧 分隔符*/
+    private static final String SPLIT = "5f";
+
+    /**
+     * 查询设备参数
+     * @param  reqInfo   请求参数信息
+     */
+    @Override
+    public void queryPara(FrameReqData reqInfo) {
+        log.info("C中频切换矩阵参数查询执行！");
+        socketMutualService.request(reqInfo, ProtocolRequestEnum.QUERY);
+    }
+
+    /**
+     * 查询设备参数响应
+     * @param  respData   数据传输对象
+     * @return
+     */
+    @Override
+    public FrameRespData queryParaResponse(FrameRespData respData) {
+        log.info("C中频切换矩阵参数查询响应执行！");
+        String bytesData = HexUtil.encodeHexStr(respData.getParamBytes());
+        //全查询：按容器中的参数顺序解析
+        String devType = respData.getDevType();
+        List<FrameParaInfo> frameParaInfos = BaseInfoContainer.getInterLinkParaList(devType,respData.getCmdMark());
+        List<FrameParaData> frameParaDataList = new ArrayList<>();
+        for (FrameParaInfo frameParaInfo : frameParaInfos){
+            //参数下标--->参数下标+参数字节长度+关键字（2）
+            String data = bytesData.substring(frameParaInfo.getParaStartPoint(),frameParaInfo.getParaStartPoint()+Integer.valueOf(frameParaInfo.getParaByteLen())+2);
+            String paraCmk = data.substring(0, 2);
+            String paraValueStr = data.substring(2);
+            byte[] paraValBytes = HexUtil.decodeHex(paraValueStr);
+            FrameParaInfo currentPara = BaseInfoContainer.getParaInfoByCmd(devType, paraCmk);
+            if (StringUtils.isEmpty(currentPara.getParaNo())){ continue;}
+            FrameParaData frameParaData = FrameParaData.builder()
+                    .devType(devType)
+                    .devNo(respData.getDevNo())
+                    .paraNo(currentPara.getParaNo())
+                    .build();
+            //根据是否为String类型采取不同的处理方式
+            boolean isStr = MonitorConstants.STRING_CODE.equals(currentPara.getDataType());
+            if (isStr){
+                frameParaData.setParaVal(paraValueStr);
+            }else {
+                //单个参数值转换
+                frameParaData.setParaVal(byteToNumber(paraValBytes, 0,
+                        Integer.parseInt(currentPara.getParaByteLen())
+                        ,isUnsigned(sysParamService, currentPara.getDataType())
+                        ,isFloat(sysParamService, currentPara.getDataType())
+                ).toString());
+            }
+            frameParaDataList.add(frameParaData);
+            //终止条件
+            if(data.contains(bytesData.substring(bytesData.length()-4))){
+                //则直接退出
+                break;
+            }
+        }
+        respData.setFrameParaList(frameParaDataList);
+        //参数查询响应结果接收
+        dataReciveService.paraQueryRecive(respData);
+        return respData;
+    }
+}
--- src/main/java/com/xy/netdev/frame/service/czp/CzpPrtcServiceImpl.java
+++ src/main/java/com/xy/netdev/frame/service/czp/CzpPrtcServiceImpl.java
@@ -11,7 +11,6 @@ import com.xy.netdev.frame.bo.FrameRespData;
 import com.xy.netdev.frame.service.IParaPrtclAnalysisService;
 import com.xy.netdev.frame.service.SocketMutualService;
 import com.xy.netdev.monitor.bo.FrameParaInfo;
-import com.xy.netdev.monitor.constant.MonitorConstants;
 import com.xy.netdev.sendrecv.enums.ProtocolRequestEnum;
 import com.xy.netdev.transit.IDataReciveService;
 import lombok.extern.slf4j.Slf4j;
@@ -22,11 +21,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.xy.netdev.common.util.ByteUtils.byteToNumber;
-import static com.xy.netdev.frame.service.gf.GfPrtcServiceImpl.isFloat;
-import static com.xy.netdev.frame.service.gf.GfPrtcServiceImpl.isUnsigned;
 
 /**
  * C中频切换矩阵
@@ -44,70 +38,15 @@ public class CzpPrtcServiceImpl  implements IParaPrtclAnalysisService {
     private IDataReciveService dataReciveService;
     @Autowired
     private ISysParamService sysParamService;
-    /**查询应答帧 分隔符*/
-    private static final String SPLIT = "5f";
 
-    /**
-     * 查询设备参数
-     * @param  reqInfo   请求参数信息
-     */
     @Override
     public void queryPara(FrameReqData reqInfo) {
-        log.info("C中频切换矩阵参数查询执行！");
-        if(reqInfo.getFrameParaList() == null && reqInfo.getFrameParaList().isEmpty()){
-            log.info("C中频切换矩阵无参数，全查询取消！");
-            return ;
-        }
-        //此处需确认关键字是否为参数序号，是否需要设置参数的字节长度
-        List<byte[]> bytes = reqInfo.getFrameParaList().stream()
-                .filter(frameParaData -> StringUtils.isNotBlank(frameParaData.getParaNo()))
-                .map(frameParaData->ByteUtils.objToBytes(frameParaData.getDevNo(),frameParaData.getLen()))
-                .collect(Collectors.toList());
-        reqInfo.setParamBytes(ByteUtils.listToBytes(bytes));
-        socketMutualService.request(reqInfo, ProtocolRequestEnum.QUERY);
+
     }
 
-    /**
-     * 查询设备参数响应
-     * @param  respData   数据传输对象
-     * @return
-     */
     @Override
     public FrameRespData queryParaResponse(FrameRespData respData) {
-        String bytesData = HexUtil.encodeHexStr(respData.getParamBytes());
-        String[] dataList = bytesData.toLowerCase().split(SPLIT.toLowerCase());
-        String devType = respData.getDevType();
-        //拆分后根据关键字获取参数
-        List<FrameParaData> frameParaDataList = new ArrayList<>();
-        for (String data : dataList) {
-            String paraCmk = data.substring(0, 2);
-            String paraValueStr = data.substring(2);
-            byte[] paraValBytes = HexUtil.decodeHex(paraValueStr);
-            FrameParaInfo currentPara = BaseInfoContainer.getParaInfoByCmd(devType, paraCmk);
-            if (StringUtils.isEmpty(currentPara.getParaNo())){ continue;}
-            FrameParaData frameParaData = FrameParaData.builder()
-                    .devType(devType)
-                    .devNo(respData.getDevNo())
-                    .paraNo(currentPara.getParaNo())
-                    .build();
-            //根据是否为String类型采取不同的处理方式
-            boolean isStr = MonitorConstants.STRING_CODE.equals(currentPara.getDataType());
-            if (isStr){
-                frameParaData.setParaVal(paraValueStr);
-            }else {
-                //单个参数值转换
-                frameParaData.setParaVal(byteToNumber(paraValBytes, 0,
-                        Integer.parseInt(currentPara.getParaByteLen())
-                        ,isUnsigned(sysParamService, currentPara.getDataType())
-                        ,isFloat(sysParamService, currentPara.getDataType())
-                ).toString());
-            }
-            frameParaDataList.add(frameParaData);
-        }
-        respData.setFrameParaList(frameParaDataList);
-        //参数查询响应结果接收
-        dataReciveService.paraQueryRecive(respData);
-        return respData;
+        return null;
     }
 
     /**
