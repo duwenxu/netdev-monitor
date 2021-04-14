3fdf181b9485807c19a8baa92c740e6bca44427f
sunchao <1306519243@qq.com>
2021-04-14 08:53:06 +0800

    增加告警推送和查询两接口

--- src/main/java/com/xy/netdev/monitor/service/IAlertInfoService.java
+++ src/main/java/com/xy/netdev/monitor/service/IAlertInfoService.java
@@ -2,6 +2,7 @@ package com.xy.netdev.monitor.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xy.netdev.monitor.entity.AlertInfo;
+import java.util.List;
 
 /**
  * 告警信息 服务类
@@ -11,4 +12,11 @@ import com.xy.netdev.monitor.entity.AlertInfo;
  */
 public interface IAlertInfoService extends IService<AlertInfo> {
 
+    /**
+     * 查询指定设备时间范围内的告警信息
+     * @param devNo
+     * @param startTime
+     * @param endTime
+     */
+    List<AlertInfo> queryAlterInfoByDevNoTime(String devNo, String startTime, String endTime);
 }
