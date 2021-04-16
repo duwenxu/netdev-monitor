3694f93a43cc27005f09cc61f4aa3bf03cfe7dbd
wenxu du <1462289331@qq.com>
2021-04-16 16:36:45 +0800

    初步提交变频器设备代码

--- src/main/java/com/xy/netdev/frame/service/codec/Hex2DecParamCodec.java
+++ src/main/java/com/xy/netdev/frame/service/codec/Hex2DecParamCodec.java
@@ -21,9 +21,19 @@ public class Hex2DecParamCodec implements ParamCodec {
             divideValue = Math.pow(10, (int) objects[0]);
         }
         long originValue = Long.parseLong(HexUtil.encodeHexStr(bytes), 16);
-        return originValue / divideValue + "";
+        if (divideValue != 1) {
+            return originValue / divideValue + "";
+        } else {
+            return originValue + "";
+        }
     }
 
+    /**
+     *
+     * @param value   编码值
+     * @param objects obj[0] 保留小数位数  obj[1]转换后字节数组的长度
+     * @return
+     */
     @Override
     public byte[] encode(String value, Object... objects) {
         //默认不做小数点保留
@@ -31,16 +41,21 @@ public class Hex2DecParamCodec implements ParamCodec {
         if (objects != null && objects.length != 0) {
             rideValue = Math.pow(10, (int) objects[0]);
         }
-        String hexStr = String.format("%08X", (long) (Double.parseDouble(value) * rideValue));
+        String format = "%08X";
+        if (objects != null && objects.length > 1 && objects[1] != null) {
+            format = "%0" + objects[1] + "X";
+        }
+        String hexStr = String.format(format, (long) (Double.parseDouble(value) * rideValue));
         return HexUtil.decodeHex(hexStr);
     }
 
     public static void main(String[] args) {
-        byte[] bytes = {0x01, 0x43, 0x15, (byte) 0x8D};
+        byte[] bytes = {0x13, (byte) 0x88};
+//        byte[] bytes = {(byte)0xC0, (byte) 0xA8,(byte)0x0A,(byte)0x16};
         Hex2DecParamCodec hex2DecParamCodec = new Hex2DecParamCodec();
-        String decode = hex2DecParamCodec.decode(bytes, 3);
+        String decode = hex2DecParamCodec.decode(bytes,0);
         System.out.println(decode);
-        byte[] encode = hex2DecParamCodec.encode(decode, 3);
-        System.out.println(encode);
+        byte[] encode = hex2DecParamCodec.encode(decode,0,4);
+        System.out.println(HexUtil.encodeHexStr(encode));
     }
 }
