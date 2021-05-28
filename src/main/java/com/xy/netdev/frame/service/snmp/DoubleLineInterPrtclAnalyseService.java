f4581113015fa5418ca78ab3a702ed53ddba4939
duwx <1462289331@qq.com>
2021-05-28 18:03:47 +0800

    提交备复线设备代码

---
+++ src/main/java/com/xy/netdev/frame/service/snmp/DoubleLineInterPrtclAnalyseService.java
@@ -0,0 +1,25 @@
+package com.xy.netdev.frame.service.snmp;
+
+import com.xy.netdev.frame.bo.FrameReqData;
+import com.xy.netdev.frame.bo.FrameRespData;
+import com.xy.netdev.frame.service.IQueryInterPrtclAnalysisService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 被复线SNMP接口查询实现
+ */
+@Service
+@Slf4j
+public class DoubleLineInterPrtclAnalyseService implements IQueryInterPrtclAnalysisService {
+
+    @Override
+    public void queryPara(FrameReqData reqInfo) {
+
+    }
+
+    @Override
+    public FrameRespData queryParaResponse(FrameRespData respData) {
+        return null;
+    }
+}
