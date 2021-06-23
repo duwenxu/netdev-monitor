c75d459e9813d502fcf1cf125c9b52a1c93b05ef
duwx <1462289331@qq.com>
2021-06-23 13:16:44 +0800

    提交ACU更改

---
+++ src/main/java/com/xy/netdev/frame/service/codec/SignedFloatCodec.java
@@ -0,0 +1,24 @@
+package com.xy.netdev.frame.service.codec;
+
+import com.xy.netdev.frame.service.ParamCodec;
+import com.xy.netdev.transit.schedule.FloatAndByte;
+import org.springframework.stereotype.Component;
+
+/**
+ * 4字节转带符号 浮点数
+ */
+@Component
+public class SignedFloatCodec implements ParamCodec {
+
+    @Override
+    public String decode(byte[] bytes, Object... objects) {
+        float v = FloatAndByte.bytesToFloat(bytes);
+        return v+"";
+    }
+
+    @Override
+    public byte[] encode(String value, Object... objects) {
+        byte[] bytes = FloatAndByte.floatToBytes(Float.parseFloat(value));
+        return bytes;
+    }
+}
