791daea805b17bbef778478762b7bbc702e11a35
duwenxu <1462289331@qq.com>
2021-07-15 18:40:58 +0800

    提交调试400W电台

--- src/main/java/com/xy/netdev/frame/service/shortwave/ShortWaveParaServiceImpl.java
+++ src/main/java/com/xy/netdev/frame/service/shortwave/ShortWaveParaServiceImpl.java
@@ -1,10 +1,22 @@
 package com.xy.netdev.frame.service.shortwave;
 
+import cn.hutool.core.util.StrUtil;
+import com.xy.netdev.frame.bo.FrameParaData;
 import com.xy.netdev.frame.bo.FrameReqData;
 import com.xy.netdev.frame.bo.FrameRespData;
 import com.xy.netdev.frame.service.IParaPrtclAnalysisService;
+import com.xy.netdev.frame.service.SocketMutualService;
+import com.xy.netdev.sendrecv.enums.ProtocolRequestEnum;
+import com.xy.netdev.transit.impl.DataReciveServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.xy.netdev.common.util.ByteUtils.listToBytes;
 
 /**
  * @Desc
@@ -14,10 +26,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class ShortWaveParaServiceImpl implements IParaPrtclAnalysisService {
+    @Autowired
+    private SocketMutualService socketMutualService;
+    @Autowired
+    private DataReciveServiceImpl dataReciveService;
 
     @Override
     public void queryPara(FrameReqData reqInfo) {
-
     }
 
     @Override
@@ -27,7 +42,19 @@ public class ShortWaveParaServiceImpl implements IParaPrtclAnalysisService {
 
     @Override
     public void ctrlPara(FrameReqData reqInfo) {
-
+        List<FrameParaData> paraList = reqInfo.getFrameParaList();
+        if (paraList == null || paraList.isEmpty()) {
+            return;
+        }
+        //控制参数信息拼接
+        FrameParaData paraData = paraList.get(0);
+        String paraVal = paraData.getParaVal();
+        byte[] dataBytes = null;
+        if (!StringUtils.isBlank(paraVal)) {
+            dataBytes = StrUtil.bytes(paraVal);
+        }
+        reqInfo.setParamBytes(dataBytes);
+        socketMutualService.request(reqInfo, ProtocolRequestEnum.CONTROL);
     }
 
     @Override
