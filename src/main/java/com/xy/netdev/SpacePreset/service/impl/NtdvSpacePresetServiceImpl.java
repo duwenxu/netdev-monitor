ebb8889d17018a5a6e10ef5b4a216c31b018a212
xy_sunchao <1306519243@qq.com>
2021-07-30 10:48:53 +0800

    完善修改信标频率后本振的自适应调整

--- src/main/java/com/xy/netdev/SpacePreset/service/impl/NtdvSpacePresetServiceImpl.java
+++ src/main/java/com/xy/netdev/SpacePreset/service/impl/NtdvSpacePresetServiceImpl.java
@@ -4,12 +4,14 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xy.netdev.SpacePreset.entity.NtdvSpacePreset;
 import com.xy.netdev.SpacePreset.mapper.NtdvSpacePresetMapper;
 import com.xy.netdev.SpacePreset.service.INtdvSpacePresetService;
+import com.xy.netdev.admin.entity.SysParam;
 import com.xy.netdev.admin.service.ISysParamService;
 import com.xy.netdev.transit.IDevCmdSendService;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import static com.xy.netdev.common.constant.SysConfigConstant.*;
 
 /**
  * <p>
@@ -35,9 +37,28 @@ public class NtdvSpacePresetServiceImpl extends ServiceImpl<NtdvSpacePresetMappe
     @Override
     public void keyStarByPolar(NtdvSpacePreset spacePreset) {
         String value = spacePreset.getSpBeaconFrequency();
+        //通过信标频率设置本振值：每个本振对应一个信标频率
+        SysParam  sysParamA= sysParamService.getById(LOCAL_VIBRATE_A);
+        SysParam  sysParamB= sysParamService.getById(LOCAL_VIBRATE_B);
+        SysParam  sysParamC= sysParamService.getById(LOCAL_VIBRATE_C);
+        SysParam  sysParamD= sysParamService.getById(LOCAL_VIBRATE_D);
+        String localVibr = sysParamService.getParaName(spacePreset.getSpLocalOscillator());
+        if(Integer.valueOf(sysParamA.getRemark2()) < Integer.valueOf(value) && Integer.valueOf(value)< Integer.valueOf(sysParamA.getRemark3())){
+            //如在A本振范围内则设置本振为A
+            localVibr = sysParamService.getParaName(LOCAL_VIBRATE_A);
+        }else if(Integer.valueOf(sysParamB.getRemark2()) < Integer.valueOf(value) && Integer.valueOf(value)< Integer.valueOf(sysParamB.getRemark3())){
+            //如在A本振范围内则设置本振为B
+            localVibr = sysParamService.getParaName(LOCAL_VIBRATE_B);
+        }else if(Integer.valueOf(sysParamC.getRemark2()) < Integer.valueOf(value) && Integer.valueOf(value)< Integer.valueOf(sysParamC.getRemark3())){
+            //如在A本振范围内则设置本振为C
+            localVibr = sysParamService.getParaName(LOCAL_VIBRATE_C);
+        }else if(Integer.valueOf(sysParamD.getRemark2()) < Integer.valueOf(value) && Integer.valueOf(value)< Integer.valueOf(sysParamD.getRemark3())){
+            //如在A本振范围内则设置本振为D
+            localVibr = sysParamService.getParaName(LOCAL_VIBRATE_D);
+        }
         //1.执行设置本振及频率
         if(StringUtils.isNotEmpty(spacePreset.getSpLocalOscillator())){
-            value = "{Z}["+sysParamService.getParaName(spacePreset.getSpLocalOscillator())+"]{F}["+value+"]";
+            value = "{Z}["+localVibr+"]{F}["+value+"]";
         }
         devCmdSendService.paraCtrSend(spacePreset.getDevNo(),"cmdsj", value);
         //2.执行一键对星
